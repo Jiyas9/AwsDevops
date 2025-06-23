7ecb2b93d71868db37b689590615447490db39cd
Jiyas9 <159109054+Jiyas9@users.noreply.github.com>
2025-06-23 20:34:19 +0530

    Add files via upload

---
+++ DataTypes.tf
@@ -0,0 +1,38 @@
+variable "Instancetype" {
+  type = number
+}
+output "NumberDataType" {
+ value = var.Instancetype
+/*You need to provide the instancetype value as number as we defined Data type as a number */
+}
+
+variable "AmiVariable" {
+  type = string
+}
+output "StringDataType" {
+ value = var.AmiVariable
+/*You need to provide the instancetype value as string as we defined Data type as a string such as "jkjhj"*/
+}
+
+variable "SshPort" {
+  type = list(number)
+}
+
+output "ListDataType" {
+ value = var.SshPort
+
+/*You need to provide the instancetype value as list as we defined Data type as a list such as [123,"Jiya"]*/
+
+}
+
+variable "httpPort" {
+  type = map(number)
+}
+
+output "MapDataType" {
+ value = var.httpPort
+
+/*You need to provide the instancetype value as string as we defined Data type as a map such as { "name = "Myname" , "DOB" = "12345"}
+*/
+
+}
